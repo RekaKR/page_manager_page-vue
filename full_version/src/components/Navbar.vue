e0f0b2454d1dbd1937a6c6f076bff6f6fc05d826
Reka Kupsza <khusire@gmail.com>
2023-05-27 20:02:35 +0200

    Update

---
+++ full_version/src/components/Navbar.vue
@@ -0,0 +1,45 @@
+<template>
+  <nav :class="[`navbar-${theme}`, `bg-${theme}`, 'navbar', 'navbar-expand-lg']">
+    <div class="container-fluid">
+      <a href="#" class="navbar-brand">My Vue</a>
+
+      <ul class="navbar-nav me-auto mb-2 mb-lg-8">
+        <li class="nav-item" v-for='(page, index) in pages' :key="index">
+          <navbar-linik :page="page" :isActive="activePage === index" @click.prevent="navLinkClick(index)"></navbar-linik>
+        </li>
+      </ul>
+
+      <form class="d-flex">
+        <button class="btn btn-primary" @click.prevent="changeTheme()">Toggle</button>
+      </form>
+    </div>
+  </nav>
+</template>
+
+<script>
+import NavbarLink from './NavbarLink.vue';
+
+export default {
+  components: {
+    NavbarLink
+  },
+  props: ['pages', 'activePage', 'navLinkClick'],
+  data() {
+    return {
+      theme: 'light',
+    }
+  },
+  methods: {
+    changeTheme() {
+      /*let bla = 'light'
+      
+      if (this.theme === 'light') {
+        bla = 'dark'
+      }
+      
+      this.theme = bla*/
+      this.theme = this.theme === 'light' ? 'dark' : 'light'
+    }
+  }
+}
+</script>
---
+++ full_version/src/components/NavbarLink.vue
@@ -0,0 +1,21 @@
+<template>
+  <a class="nav-link" :class="activeClasses" :href="page.link.url" :title="`This Link goes to the ${page.link.text} page`"
+    aria-current="page">{{
+      page.link.text }}
+  </a>
+</template>
+
+<script>
+export default {
+  props: ['page', 'isActive'],
+  computed: {
+    activeClasses() {
+      return {
+        active: this.isActive,
+        emphasize: this.Active
+      }
+    }
+  }
+
+}
+</script>
